7e306c5564d0e0288a09ee402c28b7bd8689e690
ioansen <ioan.sndr@gmail.com>
2018-10-14 20:27:53 +0300

    renamed folder

---
+++ _3BabySteps/1-2-hello-world/hello_world.asm
@@ -0,0 +1,28 @@
+%include "io.inc"
+
+section .data
+    myString: db "Hello, World!",0
+    goodbye: db "Goodbye, World!",0
+
+section .text
+global CMAIN
+CMAIN:
+    mov ecx, 6                 ; N = valoarea registrului ecx
+    mov eax, 2
+    mov ebx, 1
+    cmp eax, ebx
+    jg print                   ; TODO1: eax > ebx?
+    ret
+print:
+    PRINT_STRING myString
+    NEWLINE
+    sub ecx,1
+    test ecx, ecx
+    jnz print
+    
+    PRINT_STRING goodbye
+    NEWLINE
+                               ; TODO2.2: afisati "Hello, World!" de N ori
+                               ; TODO2.1: afisati "Goodbye, World!"
+
+    ret
