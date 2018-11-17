65cac93d6b5e7f4456535d3757878c6811e40ae1
ioansen <ioan.sndr@gmail.com>
2018-11-17 15:57:46 +0200

    added 5-print-reverse-string

--- _8Functions/4-5-print-string-len/print-string-len.asm
+++ _8Functions/4-5-print-string-len/print-string-len.asm
@@ -5,10 +5,34 @@ extern printf
 section .data
     mystring db "This is my string", 0
     length db "String length is %u", 10, 0
+    store_string times 64 db 0
 
 section .text
 global CMAIN
 
+reverse_string:
+    push ebp
+    mov ebp, esp
+
+    mov eax, dword [ebp+8]
+    mov ecx, dword [ebp+12]
+    add eax, ecx
+    dec eax
+    mov edx, dword [ebp+16]
+
+copy_one_byte:
+    mov bl, byte [eax]
+    mov byte [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    leave
+    ret
+    
 CMAIN:
     push ebp
     mov ebp, esp
@@ -27,11 +51,21 @@ out:
     PRINT_DEC 4, ecx
     NEWLINE
     push ecx
+    push ecx
     push length
     call printf
     add esp,8
+    pop ecx
     
+    push store_string
+    push ecx
+    push mystring
+    call reverse_string
+    add esp,12
 
+    push store_string
+    call puts
+    add esp, 4
 
     leave
     ret
