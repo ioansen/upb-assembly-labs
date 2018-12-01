f75deacb700683392100411bfeec43808e34c511
ioansen <ioan.sndr@gmail.com>
2018-12-01 02:06:40 +0200

    task 3

--- _T2Cryptography/tema2.asm
+++ _T2Cryptography/tema2.asm
@@ -14,11 +14,109 @@ usage:             db "Usage: %s <task-no> (task-no can be 1,2,3,4,5,6)", 10, 0
 error_no_file:     db "Error: No input file %s", 10, 0
 error_cannot_read: db "Error: Cannot read input file %s", 10, 0
 
-print_format: db "%x",10,0
-print: db "%x"
+print_format: db "some %x",10,0
+print: db "%x ", 0
+print_newline: db 10,0
 section .text
 global main
 
+;--------------------------------
+;int print_contents(*adress)
+;--------------------------------
+print_contents:    
+        enter 0, 0
+        
+        push edi
+        mov edi, [ebp + 8]
+        
+        push edi
+        call strlen             ;length in eax
+        add esp, 4
+        
+        push eax
+        mov ecx, eax
+        
+        push ecx
+        push ecx
+        push print_format
+        call printf
+        add esp, 8
+        pop ecx
+        
+
+do_the_print:
+        push ecx
+        xor edx, edx
+        mov dl, byte[edi]
+        push edx
+        push print
+        call printf
+        add esp, 8  
+        inc edi
+        pop ecx
+        loop do_the_print
+                
+        push print_newline
+        call printf
+        add esp, 4
+        
+        pop eax                 ;return length
+        pop edi
+        leave
+        ret
+;--------------------------------
+;int * convert_hex_bin(*adress)
+;--------------------------------
+convert_hex_bin:
+        enter 0, 0
+        
+        push edi
+        push esi
+        
+        mov edi, [ebp + 8]      ;the adress of string to convert
+        mov esi, edi
+        
+        push edi
+        call strlen             ;length in eax
+        add esp, 4
+        
+        mov ecx, eax
+        shr ecx,1               ;we'll be going in 2's so ecx /= 2
+
+do_the_convert:                 ;convert by translating byte to hex symbol
+                                ;then trasnlating next one then merging the two
+        xor edx, edx
+        
+        mov dl, byte[esi]
+        sub dl, '0'
+        cmp dl, 9
+        jbe good1
+        sub dl, 0x27            ;0x27 = 0x61 - 0x30 + 0xa ; 'a'-'0' + 10
+good1:        
+        mov dh, dl
+        shl dh,4       
+        inc esi
+        
+        mov dl, byte[esi]
+        sub dl, '0'
+        cmp dl, 9
+        jbe good2
+        sub dl, 0x27   
+good2: 
+        xor dl, dh
+        xor dh, dh  
+        inc esi 
+        mov byte[edi], dl
+        inc edi        
+        loop do_the_convert
+        
+        
+        mov eax, [ebp + 8]
+        pop esi                 ;restore esi and edi
+        pop edi
+        leave
+        ret
+        
 ;--------------------------------
 ;int * get_key_address(*adress)
 ;--------------------------------
@@ -28,16 +126,18 @@ get_key_address:
         mov edi, [ebp + 8]
         
                                 ;assume cld
-        xor al, al              ;mov ax, 0
+        xor eax, eax            ;mov ax, 0
         mov ecx, 50             ;scan 50 bytes
         repne scasb             
         
         mov eax, edi
         leave
         ret
+
         
+                        
 ;---------------------------------
-;void xor_strings(*string, *key)   
+;void xor_strings(*key, *string)
 ;---------------------------------     
 xor_strings:
                                 ;TODO TASK 1
@@ -93,9 +193,34 @@ do_the_rolling:
         leave
         ret
 
+;----------------------------------------
+;void xor_hex_strings(*key, *string);
+;----------------------------------------
 xor_hex_strings:
-	; TODO TASK 3
-	ret
+                                ;TODO TASK 3
+        enter 0,0
+               
+        
+        mov esi, [ebp + 8]      ;the key
+        mov edi, [ebp + 12]     ;the string
+        
+        push edi
+        call convert_hex_bin    ;string start address in eax
+        add esp, 4
+        
+        push eax                ;save starting string address on stack
+        
+        push esi
+        call convert_hex_bin    ;key start adress in eax
+        add esp, 4
+        
+        push eax                ;save starting key address on stack
+
+        call xor_strings
+        add esp, 8
+        
+        leave
+        ret
 
 base32decode:
 	; TODO TASK 4
@@ -178,58 +303,62 @@ main:
 	jmp task_done
 
 task1:
-                                    ;TASK 1: Simple XOR between two byte streams
-
-        
+                                    ;TASK 1: Simple XOR between two byte streams       
                                     ;TODO TASK 1: find the address for the string and the key
-        push ecx                    ;save address on stack for puts
-        push ecx                    ;save again for xor_strings
+                                    
         
         push ecx                    ;send as param to get_key_address
-        call get_key_address        ;key address in eax
-        add esp,4     
-        
+        call get_key_address        ;key address in eax    
+                                    ;leave ecx on stack    
         
                                     ;TODO TASK 1: call the xor_strings function
         
                                     ;ecx already on stack
         push eax                    ;the key address
         call xor_strings
-        add esp, 8
+        add esp, 4
        
                                     ;ecx already on stack
         call puts                   ;print resulting string
-        add esp, 4
+        add esp, 4                  ;release ecx
         
-	jmp task_done
+        jmp task_done
 
 task2:
                                     ;TASK 2: Rolling XOR
 
-                                    ;TODO TASK 2: call the rolling_xor function
-        push ecx                    ;save string address
-        
-        push ecx
+                                    ;TODO TASK 2: call the rolling_xor function        
+        push ecx                    ;save string address on stack
         call rolling_xor
-        add esp,4
         
                                     ;ecx already on stack
-        call puts
-        add esp, 4
+        call puts                   ;print resulting string
+        add esp, 4                  ;release ecx
 
         jmp task_done
 
 task3:
-	; TASK 3: XORing strings represented as hex strings
+                                    ;TASK 3: XORing strings represented as hex strings
+        
+        mov ebx, ecx                ;save in ebx because strlen shouldn't modify it
+        push ecx                    ;save adress and send as param        
+        call strlen                 ;length in eax
+        
+        add ebx, eax                ;compute key address as base + length + 1 (1 from 00) 
+        inc ebx
+             
+                                    ;ecx already on stack
+        push ebx
+        call xor_hex_strings
+        add esp, 4
+        
+                                    ;ecx already on stack
+        call puts                   ;print resulting string
+        add esp, 4                  ;relese ecx  
+        
+        
+        jmp task_done
 
-	; TODO TASK 1: find the addresses of both strings
-	; TODO TASK 1: call the xor_hex_strings function
-
-	push ecx                     ;print resulting string
-	call puts
-	add esp, 4
-
-	jmp task_done
 
 task4:
 	; TASK 4: decoding a base32-encoded string
