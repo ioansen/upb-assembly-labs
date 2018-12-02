1d94ee0e153c70c4aa92f1cdab65d48097b905d5
ioansen <ioan.sndr@gmail.com>
2018-12-02 03:08:13 +0200

    task 5

--- _T2Cryptography/tema2.asm
+++ _T2Cryptography/tema2.asm
@@ -21,20 +21,26 @@ base32_alphapet: db "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567="
 section .text
 global main
 
-;--------------------------------
-;int print_contents(*adress)
-;--------------------------------
+;--------------------------------------------------------------------------------
+;void print_contents(*adress)    
+;--------------------------------------------------------------------------------
+;helper print method that prints bytes from an adress untill byte 0
+;it's just for testing purposes and it shouldn't be called in the final prgram
+;uses pusha at at the begining so it can be easily used
+;--------------------------------------------------------------------------------
 print_contents:    
         enter 0, 0
         
-        push esi
+        pusha                   
         mov esi, [ebp + 8]
         
         push esi
         call strlen             ;length in eax
         add esp, 4
+                
+        test eax, eax
+        jz out_print
         
-        push eax
         mov ecx, eax
                 
 do_the_print:
@@ -44,15 +50,16 @@ do_the_print:
         push eax
         push print
         call printf
-        add esp, 12 
+        add esp, 8 
+        pop ecx
         loop do_the_print
                 
         push print_newline
         call printf
         add esp, 4
-        
-        pop eax                 ;return length
-        pop esi
+
+out_print:                
+        popa
         leave
         ret
 
@@ -112,6 +119,63 @@ do_the_convertion:              ;convert by translating byte to hex symbol then 
         
         pop esi                 ;restore esi and edi
         pop edi
+        leave
+        ret
+   
+;----------------------------------------------------------------
+;int contains(*substring, int length, *string)
+;----------------------------------------------------------------        
+contains:         
+        enter 0,0
+        
+        push ebx                    ;save the register we use
+        push esi                    ;so we can restore
+        push edi
+        
+        mov edi, [ebp + 8]          ;the string we're looking for  
+        mov esi, [ebp + 12]         ;the string we're searching in
+       
+        push esi
+        call strlen                 ;compute string length
+        add esp, 4
+        
+        test eax, eax               ;test if no string and just exit if so
+        jz contains_out
+                
+        push eax                    ;save string length on stack, use eax for next strlen
+
+        mov ebx, edi                ;substring base at ebx
+               
+        push edi
+        call strlen                 ;substring length in eax
+        add esp, 4
+        
+        pop edx                     ;string length in edx 
+        
+do_the_iteration:
+        push esi
+        mov edi, ebx                ;move edi back to substring address        
+        xor ecx, ecx
+        mov cl, al                  ;prepare cl for cmpsb(set it to substring length)
+        repe cmpsb
+        jne mismatch
+match:
+        add esp, 4                  ;release esi
+        mov eax, 1                  ;return 1 if match
+        jmp contains_out
+mismatch:
+        pop esi                     ;get start adress back
+        inc esi                     ;add 1 so newxt we verify it from next char              
+        dec dl                      ;always dec dl 1 since we inc esi with 1
+        test dl, dl
+        jnz do_the_iteration
+        
+        xor eax, eax                ;return 0 if mismatch
+
+contains_out:       
+        pop edi                     ;restore used registers
+        pop esi 
+        pop ebx
         leave
         ret
         
@@ -323,87 +387,199 @@ add_padding:
         leave
         ret
 
-;-----------------------------------------------------------
-;int bruteforce_singlebyte_xor(char *encode_string);
-;-----------------------------------------------------------
+;------------------------------------------------------------
+;int bruteforce_singlebyte_xor(*string);
+;------------------------------------------------------------
+;a byte has 256 possible values
+;just search them all
+;so for every byte, generate key by multiplying byte
+;then xor them strings, then check if string contains 'force'
+;sounds like bruteforce
+;------------------------------------------------------------
 bruteforce_singlebyte_xor:
-                                   ;TODO TASK 5
+                                    ;TODO TASK 5
         enter 0,0
         
+        push esi                    ;save used registers
+        push edi
+        push ebx
+        
+        mov esi, [ebp + 8]          ;string address in esi
+               
+        push esi
+        call strlen                 ;length in eax
+        add esp, 4
+        
+        mov ecx, eax                ;string length in ecx
+        
+        ;sub esp, ecx                ;reserve space on stack for key
+        ;mov edi, esp
+        
+        sub esp, 6                  ;leave space to also put 'force' on stack ('force' should be param so we could mass use this method/procedure)
+        mov byte[esp], 'f'
+        mov byte[esp + 1], 'o'
+        mov byte[esp + 2], 'r'
+        mov byte[esp + 3], 'c'
+        mov byte[esp + 4], 'e'
+        mov byte[esp + 5], 0
+        mov ebx, esp                ;'force' address in ebx, ebx saved by procedures so we won't have to push it everytime
+        
+        ;push ecx                    ;push length so we can restore stack later
+        
+        
+        mov dl, 0xff                ;generate 255, the first tested key
+        
+do_the_bruteforce:           
+        push edx                    ;save loop counter (and the key )
+        push ecx                    ;save the string length
+        
+        ;push edi
+        ;mov al, dl                  ;multiply key with stosb, store byte in al
+        ;rep stosb                   ;repeat ecx time
+        ;mov byte[edi], 0            ;set string terminator
+        ;pop edi                     ;get edi back from rep stosb        
+        ;pop ecx                     ;get ecx back from rep stosb
+
+        ;push ecx                    ;push the registers affected by xor_them
+        ;push edi
+        push esi
+        
+xor_them:                           ;xor them like in xor_strings
+                                    ;can't use xor_strings procedure
+                                    ;because i want to xor them precisely ecx times (the original strlen)
+                                    ;and not the temp strlen times
+                                    ;since i need to revert this operation
+        ;mov dl, byte[edi]
+        xor byte[esi], dl 
+        ;inc edi
+        inc esi      
+        loop xor_them
+        
+        pop esi                     ;get back registers used by xor_them
+        ;pop edi
+        pop ecx
+        ;pop edx                     ;including edx
+        
+        ;push edx                    ;push registers before contains
+        push ecx                                                  
+        
+        push esi                    
+        push ebx
+        call contains               ;eax contains boolean (0 if not found)
+        add esp, 8
+        
+        pop ecx                     ;get back registers before contains
+        pop edx
+
+        test eax, eax               ;test if contains == true
+        jnz found
+        
+        push edx                    ;push registers used by xor_the_back
+        push ecx
+        ;push edi
+        push esi
+        
+xor_them_back:                      ;revert xor_them operation
+                                    ;so we start next iteration with the initial string
+        ;mov dl, byte[edi]
+        xor byte[esi], dl 
+        ;inc edi
+        inc esi      
+        loop xor_them_back
+         
+        pop esi                     ;get back registers used by xor_the_back
+        ;pop edi
+        pop ecx             
+        pop edx
+        
+        dec dl                                                                                                  
+        test dl, 0xff                ;also test algorithm for 0 key (leave when dl = -1 i.e when dl is back to 255)
+        jnz do_the_bruteforce
+        
+found:
+        ;pop ecx                      ;get string length and redo stack
+        add esp, 6                   ;restore 'force' space
+        ;add esp, ecx                 ;restore generated key sapce
+        ;inc esp
+        
+        mov al,dl                    ;put key in al i.e return key
+        pop ebx                      ;restore edi, esi, ebx
+        pop edi                      
+        pop esi                                                 
         leave
         ret
 
 decode_vigenere:
-    ; TODO TASK 6
-    ret
+        ; TODO TASK 6
+        ret
 
 main:
-    push ebp
-    mov ebp, esp
-    sub esp, 2300
-
-    ; test argc
-    mov eax, [ebp + 8]
-    cmp eax, 2
-    jne exit_bad_arg
-
-    ; get task no
-    mov ebx, [ebp + 12]
-    mov eax, [ebx + 4]
-    xor ebx, ebx
-    mov bl, [eax]
-    sub ebx, '0'
-    push ebx
-
-    ; verify if task no is in range
-    cmp ebx, 1
-    jb exit_bad_arg
-    cmp ebx, 6
-    ja exit_bad_arg
-
-    ; create the filename
-    lea ecx, [filename + 7]
-    add bl, '0'
-    mov byte [ecx], bl
-
-    ; fd = open("./input{i}.dat", O_RDONLY):
-    mov eax, 5
-    mov ebx, filename
-    xor ecx, ecx
-    xor edx, edx
-    int 0x80
-    cmp eax, 0
-    jl exit_no_input
-
-    ; read(fd, ebp - 2300, inputlen):
-    mov ebx, eax
-    mov eax, 3
-    lea ecx, [ebp-2300]
-    mov edx, [inputlen]
-    int 0x80
-    cmp eax, 0
-    jl exit_cannot_read
-
-    ; close(fd):
-    mov eax, 6
-    int 0x80
-
-        cld
-        ; all input{i}.dat contents are now in ecx (address on stack)
-    pop eax
-    cmp eax, 1
-    je task1
-    cmp eax, 2
-    je task2
-    cmp eax, 3
-    je task3
-    cmp eax, 4
-    je task4
-    cmp eax, 5
-    je task5
-    cmp eax, 6
-    je task6
-    jmp task_done
+        push ebp
+        mov ebp, esp
+        sub esp, 2300
+    
+        ; test argc
+        mov eax, [ebp + 8]
+        cmp eax, 2
+        jne exit_bad_arg
+    
+        ; get task no
+        mov ebx, [ebp + 12]
+        mov eax, [ebx + 4]
+        xor ebx, ebx
+        mov bl, [eax]
+        sub ebx, '0'
+        push ebx
+    
+        ; verify if task no is in range
+        cmp ebx, 1
+        jb exit_bad_arg
+        cmp ebx, 6
+        ja exit_bad_arg
+    
+        ; create the filename
+        lea ecx, [filename + 7]
+        add bl, '0'
+        mov byte [ecx], bl
+    
+        ; fd = open("./input{i}.dat", O_RDONLY):
+        mov eax, 5
+        mov ebx, filename
+        xor ecx, ecx
+        xor edx, edx
+        int 0x80
+        cmp eax, 0
+        jl exit_no_input
+    
+        ; read(fd, ebp - 2300, inputlen):
+        mov ebx, eax
+        mov eax, 3
+        lea ecx, [ebp-2300]
+        mov edx, [inputlen]
+        int 0x80
+        cmp eax, 0
+        jl exit_cannot_read
+    
+        ; close(fd):
+        mov eax, 6
+        int 0x80
+    
+            cld
+            ; all input{i}.dat contents are now in ecx (address on stack)
+        pop eax
+        cmp eax, 1
+        je task1
+        cmp eax, 2
+        je task2
+        cmp eax, 3
+        je task3
+        cmp eax, 4
+        je task4
+        cmp eax, 5
+        je task5
+        cmp eax, 6
+        je task6
+        jmp task_done
 
 task1:
                                     ;TASK 1: Simple XOR between two byte streams       
@@ -462,85 +638,90 @@ task3:
         
         jmp task_done
 
-
 task4:
-    ; TASK 4: decoding a base32-encoded string
-
-    ; TODO TASK 4: call the base32decode function
+        ; TASK 4: decoding a base32-encoded string
     
-    push ecx
-    call puts                    ;print resulting string
-    add esp,4
-    
-    jmp task_done
+        ; TODO TASK 4: call the base32decode function
+        
+        push ecx
+        call puts                    ;print resulting string
+        add esp,4
+        
+        jmp task_done
 
 task5:
-    ; TASK 5: Find the single-byte key used in a XOR encoding
-
-    ; TODO TASK 5: call the bruteforce_singlebyte_xor function
-
-    push ecx                    ;print resulting string
-    call puts
-    add esp, 4
-
-    push eax                    ;eax = key value
-    push fmtstr
-    call printf                 ;print key value
-    add esp, 8
-
-    jmp task_done
+        ; TASK 5: Find the single-byte key used in a XOR encoding
+    
+        ; TODO TASK 5: call the bruteforce_singlebyte_xor function
+    
+        push ecx
+        call bruteforce_singlebyte_xor      ;key in eax
+        pop ecx
+        
+        push eax                    ;save key (before puts)
+        
+        push ecx
+        call puts                   ;print resulting string
+        add esp, 4
+        
+                                    ;eax = key value (eax alredy on stack)
+        push fmtstr
+        call printf                 ;print key value
+        add esp, 8
+        
+        jmp task_done
 
 task6:
-    ; TASK 6: decode Vignere cipher
-
-    ; TODO TASK 6: find the addresses for the input string and key
-    ; TODO TASK 6: call the decode_vigenere function
-
-    push ecx
-    call strlen
-    pop ecx
-
-    add eax, ecx
-    inc eax
-
-    push eax
-    push ecx                   ;ecx = address of input string 
-    call decode_vigenere
-    pop ecx
-    add esp, 4
-
-    push ecx
-    call puts
-    add esp, 4
+        ; TASK 6: decode Vignere cipher
+    
+        ; TODO TASK 6: find the addresses for the input string and key
+        ; TODO TASK 6: call the decode_vigenere function
+    
+        push ecx
+        call strlen
+        pop ecx
+    
+        add eax, ecx
+        inc eax
+    
+        push eax
+        push ecx                   ;ecx = address of input string 
+        call decode_vigenere
+        pop ecx
+        add esp, 4
+    
+        push ecx
+        call puts
+        add esp, 4
 
 task_done:
-    xor eax, eax
-    jmp exit
+        xor eax, eax
+        jmp exit
 
 exit_bad_arg:
-    mov ebx, [ebp + 12]
-    mov ecx , [ebx]
-    push ecx
-    push usage
-    call printf
-    add esp, 8
-    jmp exit
+        mov ebx, [ebp + 12]
+        mov ecx , [ebx]
+        push ecx
+        push usage
+        call printf
+        add esp, 8
+        jmp exit
 
 exit_no_input:
-    push filename
-    push error_no_file
-    call printf
-    add esp, 8
-    jmp exit
+        push filename
+        push error_no_file
+        call printf
+        add esp, 8
+        jmp exit
 
 exit_cannot_read:
-    push filename
-    push error_cannot_read
-    call printf
-    add esp, 8
-    jmp exit
+        push filename
+        push error_cannot_read
+        call printf
+        add esp, 8
+        jmp exit
 
 exit:
-    mov esp, ebp
-    pop ebp
-    ret
+        mov esp, ebp
+        pop ebp
+        ret
